1b3cf3e9ef9552ed176873ee773e691a0d8258c8
uimarshall <uimarshall@gmail.com>
2020-11-06 16:11:51 +0100

    Add menu page and styles

--- src/app/app.js
+++ src/app/app.js
@@ -19,60 +19,58 @@ doc = document.URL;
 // document.body.appendChild(para);
 
 const loadPage = (() => {
-    
-    
-    let content = document.querySelector('#content');
-    let nav = document.createElement('nav')
-    nav.classList.add("navbar", "navbar-expand-md", "navbar-dark", "fixed-top", "bg-dark")
-    let brand = document.createElement('a')
-    brand.classList.add('navbar-brand')
-     brand.setAttribute('href','#');
-     brand.textContent = "BeckNCall";
-     
-    let ul = document.createElement('ul');
-    ul.classList.add('nav','nav-tabs');
-    let pages = ['home','menu','contact'];
-    pages.forEach(page => {
-        let list = document.createElement('li');
-        list.classList.add('nav-item');
-        let link = document.createElement('a');
-        link.classList.add('nav-link');
-        link.setAttribute('href','#');
-        link.textContent = page;
-        list.appendChild(link);
-        ul.appendChild(list);
-        nav.appendChild(brand)
-        nav.appendChild(ul)
-    })
-   
-    content.appendChild(nav);
-    
-    let pageContent = document.createElement('div');
-    pageContent.classList.add('pageContent')
-    content.appendChild(pageContent);
-    
-    let footer = document.createElement('footer');
-    footer.classList.add('footer', 'p-5');
-    footer.innerHTML = `
+  const content = document.querySelector('#content');
+  const nav = document.createElement('nav');
+  nav.classList.add('navbar', 'navbar-expand-md', 'navbar-dark', 'fixed-top', 'bg-dark');
+  const brand = document.createElement('a');
+  brand.classList.add('navbar-brand');
+  brand.setAttribute('href', '#');
+  brand.textContent = 'BeckNCall';
+
+  const ul = document.createElement('ul');
+  ul.classList.add('nav', 'nav-tabs');
+  const pages = ['home', 'menu', 'contact'];
+  pages.forEach(page => {
+    const list = document.createElement('li');
+    list.classList.add('nav-item');
+    const link = document.createElement('a');
+    link.classList.add('nav-link');
+    link.setAttribute('href', '#');
+    link.textContent = page;
+    list.appendChild(link);
+    ul.appendChild(list);
+    nav.appendChild(brand);
+    nav.appendChild(ul);
+  });
+
+  content.appendChild(nav);
+
+  const pageContent = document.createElement('div');
+  pageContent.classList.add('pageContent');
+  content.appendChild(pageContent);
+
+  const footer = document.createElement('footer');
+  footer.classList.add('footer', 'p-5', 'mt-auto');
+  footer.innerHTML = `
     <p class="float-right"><a href="#">Back to top</a></p>
 				<p>
 					&copy; 2017-2020 Company, Inc. &middot;
 					<a href="#">Privacy</a> &middot; <a href="#">Terms</a>
 				</p>`;
-    content.appendChild(footer);
+  content.appendChild(footer);
 
 
-    return {
-        nav,
-        pageContent
-    }
+  return {
+    nav,
+    pageContent,
+  };
 })();
 
-const nav = loadPage.nav;
-const pageContent = loadPage.pageContent;
+const { nav } = loadPage;
+const { pageContent } = loadPage;
 
-export{
-    loadPage,
-    nav,
-    pageContent
-}
+export {
+  loadPage,
+  nav,
+  pageContent,
+};
---
+++ src/app/menu.js
@@ -0,0 +1,119 @@
+
+import { pageContent } from './app';
+
+const menuPage = (() => {
+  const main = document.createElement('main');
+  main.setAttribute('role', 'main');
+  const section = document.createElement('section');
+  section.classList.add('jumbotron', 'text-center');
+  const wrapper = document.createElement('div');
+  wrapper.classList.add('container');
+  const h1 = document.createElement('h1');
+  h1.textContent = 'Album example';
+  const p = document.createElement('p');
+  p.classList.add('lead', 'text-muted');
+  p.textContent = 'Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely';
+  wrapper.appendChild(h1);
+  wrapper.appendChild(p);
+  section.appendChild(wrapper);
+
+  const tiles = document.createElement('div');
+  tiles.classList.add('album', 'py-5', 'bg-light');
+  tiles.innerHTML = `  <div class="container">
+  
+
+      <div class="row">
+        <div class="col-md-4">
+          <div class="card mb-4 shadow-sm">
+          <img src="https://cdn.pixabay.com/photo/2018/08/10/21/52/restaurant-3597677__340.jpg" alt="food" class="bd-placeholder-img card-img-top" />
+           
+            <div class="card-body">
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card mb-4 shadow-sm">
+            <img src="https://cdn.pixabay.com/photo/2017/02/15/10/57/pizza-2068272__340.jpg" alt="food" class="bd-placeholder-img card-img-top" />
+            <div class="card-body">
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card mb-4 shadow-sm">
+             <img src="https://cdn.pixabay.com/photo/2016/11/29/05/07/baked-goods-1867459__340.jpg" alt="food" class="bd-placeholder-img card-img-top" />
+            <div class="card-body">
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+             
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="card mb-4 shadow-sm">
+           <img src="https://cdn.pixabay.com/photo/2019/07/17/11/02/neon-sign-4343663__340.jpg" alt="food" class="bd-placeholder-img card-img-top" />
+            <div class="card-body">
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+             
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card mb-4 shadow-sm">
+            <img src="https://cdn.pixabay.com/photo/2014/12/21/23/56/hamburger-576419__340.png" alt="food" class="bd-placeholder-img card-img-top" />
+            <div class="card-body">
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card mb-4 shadow-sm">
+           <img src="https://cdn.pixabay.com/photo/2017/09/30/15/10/pizza-2802332__340.jpg" alt="food" class="bd-placeholder-img card-img-top" />
+            <div class="card-body">
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+             
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="card mb-4 shadow-sm">
+            <img src="https://cdn.pixabay.com/photo/2017/06/06/22/37/italian-cuisine-2378729__340.jpg" alt="food" class="bd-placeholder-img card-img-top" />
+            <div class="card-body">
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card mb-4 shadow-sm">
+            <img src="https://cdn.pixabay.com/photo/2017/06/26/12/49/red-wine-2443699__340.jpg" alt="food" class="bd-placeholder-img card-img-top" />
+            <div class="card-body">
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card mb-4 shadow-sm">
+           <img src="https://cdn.pixabay.com/photo/2017/01/26/02/06/platter-2009590__340.jpg" alt="food" class="bd-placeholder-img card-img-top" />
+            <div class="card-body">
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+             
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>`;
+
+
+  main.appendChild(section);
+  main.appendChild(tiles);
+  pageContent.appendChild(main);
+});
+
+export { menuPage };
